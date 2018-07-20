11fc224b66e35186a3d5b4abdcfaf4b675168d14
Guillaume Baudart <guillaume.baudart@ibm.com>
2018-07-20 14:43:07 -0400

    Clean import from yaps.lib

--- coin.py
+++ coin.py
@@ -1,5 +1,5 @@
 from yaps import parse_model
-from yaps.lib import *
+from yaps.lib import int, real, parameters, model, uniform, bernoulli
 
 
 def coin_model_py(x: int(lower=0, upper=1)[10]):
@@ -17,9 +17,9 @@ print('----------------')
 
 
 def coin_model_py2(x: int(lower=0, upper=1)[10]):
-    theta: real(lower=0, upper=1) < ~ uniform(0, 1)
+    theta: real(lower=0, upper=1) is uniform(0, 1)
     for i in range(10):
-        x[i] < ~ bernoulli(theta)
+        x[i] is bernoulli(theta)
 
 
 parse_model(coin_model_py2)
