b96224dbfae01a1455f52427e48a093004ef1661
Guillaume Baudart <guillaume.baudart@ibm.com>
2018-07-23 10:23:30 -0400

    remove unnecessary parameters def in slicstan example

--- tests/slicstan.py
+++ tests/slicstan.py
@@ -7,12 +7,10 @@ from yaps.lib import int, real, transformed_parameters, generated_quantities, ga
 
 
 def slicstan(N: int, y: real(lower=0, upper=1)[10]):
-    mu: real
-    tau: real
+    tau: real is gamma(0.1, 0.1)
+    mu: real is normal(0, 1)
     with transformed_parameters:
         sigma: real = pow(tau, -0.5)
-    tau is gamma(0.1, 0.1)
-    mu is normal(0, 1)
     y is normal(mu, sigma)
     with generated_quantities:
         v: real = pow(sigma, 2)
