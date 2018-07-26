ac975d68e81d00bce60c7157ab5fd11c0a5e1a3b
Avi Shinnar <shinnar@us.ibm.com>
2018-07-26 15:46:49 -0400

    update tests/coin.py to look like the paper version

--- tests/coin.py
+++ tests/coin.py
@@ -11,10 +11,10 @@ def coin(x: int(lower=0, upper=1)[10]):
     for i in range(10):
         x[i] is bernoulli(theta)
 
+coin.graph
 
-flips = [0, 1, 0, 0, 0, 0, 0, 0, 0, 1]
 print(coin)
 
-fit = yaps.infer(coin(x=flips),
-                iter=1000)
-print(fit)
+flips = [0, 1, 0, 0, 0, 0, 0, 0, 0, 1]
+posterior = yaps.infer(coin(x=flips), iter=1000)
+print("mean of theta: {:.3f}".format(posterior.theta.mean()))
