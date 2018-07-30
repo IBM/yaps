e57c4fd29f605a24a5ce3a630c704322c1ac73c6
Guillaume Baudart <guillaume.baudart@ibm.com>
2018-07-30 11:56:34 -0400

    Update test types

--- tests/types.py
+++ tests/types.py
@@ -8,21 +8,22 @@ import yaps as yaps
 
 N = type_var("N")
 
+
 @yaps.model
 def test(AA: int[5],
-    A: matrix(lower=0)[5, 6][6],
-    B:matrix[5, 6][6],
-    C:matrix[5, 6][6,7],
-    D:matrix(lower=0)[5, 6],
-    E:simplex[5],
-    F:simplex(lower=0)[5][5, 6],
-    G:simplex[5],
-    H:simplex(lower=0)[5],
-    I:int(upper=5)[3],
-    J:int(upper=5),
-    K:int,
-    L:int[6]
-    ):
-        alpha: real
+         A: matrix(lower=0)[5, 6][6],
+         B: matrix[5, 6][6],
+         C: matrix[5, 6][6, 7],
+         D: matrix(lower=0)[5, 6],
+         E: simplex[5]):
+    F: simplex(lower=0)[5][5, 6]
+    G: simplex[5]
+    H: simplex(lower=0)[5]
+    I: int(upper=5)[3]
+    J: int(upper=5)
+    K: int
+    L: int[6]
+    alpha: real
+
 
 print(test)
