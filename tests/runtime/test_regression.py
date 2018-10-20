a0858bf27a2692f405f6eb910673180447bf037d
kakate@us.ibm.com <kakateus.ibm.com@kakateumcomsMBP.fios-router.home>
2018-10-20 07:48:38 -0400

    cleaned up the way of testing and added test_regression

---
+++ tests/runtime/test_regression.py
@@ -0,0 +1,53 @@
+import pystan
+import numpy as np
+import matplotlib.pyplot as plt
+import yaps
+from .utils import compare_fit_objects, global_num_chains,global_num_iterations,global_random_seed
+
+
+def test_regression():
+    stan_code = """
+    data {
+        int<lower=0> N;
+        vector[N] x;
+        vector[N] y;
+        }
+    parameters {
+        real alpha;
+        real beta;
+        real<lower=0> sigma;
+    }
+    model {
+        y ~ normal(alpha + beta * x, sigma);
+        }
+    """
+
+    # Round Trip from Stan to Yaps to Stan
+    yaps_code = yaps.from_stan(code_string=stan_code)
+    generated_stan_code = yaps.to_stan(yaps_code)
+
+    # Add Data
+    num_samples = 100
+    X = np.arange(num_samples)
+    y = np.arange(num_samples)
+
+    data = {'N': num_samples,
+            'x': X,
+            'y':y}
+
+    # Compile and fit
+    sm1 = pystan.StanModel(model_code=str(stan_code))
+    fit_stan = sm1.sampling(data=data, iter=global_num_iterations, chains=global_num_chains, seed=global_random_seed)
+
+    # Compile and fit
+    sm2 = pystan.StanModel(model_code=str(generated_stan_code))
+    fit_generated_stan = sm2.sampling(data=data, iter=global_num_iterations, chains=global_num_chains, seed=global_random_seed)
+
+    compare_fit_objects(fit_stan, fit_generated_stan)
+
+    # if matplotlib is installed (optional, not required), a visual summary and
+    # traceplot are available
+    #print(fit_stan)
+    #fit.plot()
+    #plt.show()
+
---
+++ tests/runtime/utils.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+global_num_iterations=1000
+global_num_chains=4
+global_random_seed=42
+
+def compare_fit_objects(fit_stan, fit_generated_stan):
+    #Check that the number of parameters are equal
+    assert len(fit_stan._get_param_names()) == len(fit_generated_stan._get_param_names())
+    for param_name in fit_stan._get_param_names():
+        param_stan = fit_stan.extract(permuted=True)[param_name]
+        param_generated_stan = fit_generated_stan.extract(permuted=True)[param_name]
+        if(isinstance(param_stan,  np.ndarray)):
+            assert np.all(param_stan == param_generated_stan)
+        else:
+            assert param_stan == param_generated_stan
